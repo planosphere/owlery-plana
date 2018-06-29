044bcb44a6b184e504774ad7b1f4f9c2e4a8866f
Rob Court <robbie.court@gmail.com>
2018-06-29 12:24:41 +0100

    location fix

--- startup.sh
+++ startup.sh
@@ -2,7 +2,7 @@
 
 cd /opt/VFB/owlery
 
-sed -i "s|http://www.virtualflybrain.org/owl/vfb.owl|${OWLURL}|g" /opt/VFB/owlery/src/main/resources/application.conf
+sed -i "s|http://www.virtualflybrain.org/owl/vfb.owl|${OWLURL}|g" /srv/conf/application.conf
 
 
 /usr/share/owlery/bin/owlery -Dowlery.port=8080 -Dowlery.host=0.0.0.0
