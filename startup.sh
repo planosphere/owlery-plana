09bcbfa7807823ec02adf94662aa91d7fec9b6be
Rob Court <robbie.court@gmail.com>
2018-06-29 12:07:53 +0100

    adding entrypoint

--- startup.sh
+++ startup.sh
@@ -4,4 +4,5 @@ cd /opt/VFB/owlery
 
 sed -i "s|http://www.virtualflybrain.org/owl/vfb.owl|${OWLURL}|g" /opt/VFB/owlery/src/main/resources/application.conf
 
-sbt "re-start" "run"
+
+/usr/share/owlery/bin/owlery -Dowlery.port=8080 -Dowlery.host=0.0.0.0
